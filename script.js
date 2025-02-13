171b399a1530994405c3c0ffde9afc1677fcaf93
likasmr <likasmr1@gmail.com>
2025-02-14 03:10:16 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // 主题切换功能
 const themeToggle = document.getElementById('theme-toggle');
-const savedTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+const currentTheme = localStorage.getItem('theme') || 'dark';
 
-document.documentElement.setAttribute('data-theme', savedTheme);
+document.documentElement.setAttribute('data-theme', currentTheme);
 
 themeToggle.addEventListener('click', () => {
-    const currentTheme = document.documentElement.getAttribute('data-theme');
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
     document.documentElement.setAttribute('data-theme', newTheme);
     localStorage.setItem('theme', newTheme);
